29a0775144acf6ac84957c4989ef93ae4c807fef
lbm-realty <labeebmuntasir@gmail.com>
2025-10-25 15:35:23 -0500

    Training model; encountering errors

--- tokenize_rnn.py
+++ tokenize_rnn.py
@@ -1,17 +1,8 @@
 import torch
 import torch.nn as nn
-
-class SimpleRNN(nn.Module):
-    def __init__(self, vocab_size, embed_size, hidden_size):
-        super().__init__()
-        self.embedding = nn.Embedding(vocab_size, embed_size)
-        self.rnn = nn.RNN(embed_size, hidden_size, batch_first=True)
-        self.fc = nn.Linear(hidden_size, vocab_size)
-    def forward(self, x):
-        x = self.embedding(x)
-        out, _ = self.rnn(x)
-        out = self.fc(out)
-        return out
+from torch.utils.data.dataset import Dataset
+from torch.utils.data.dataloader import DataLoader
+import torch.optim as optim
 
 try: 
     with open("random_text.txt", encoding="utf-8") as file:
@@ -48,18 +39,61 @@ for letter in text:
     else:
         word += letter
 
-text_embedded = torch.tensor(text_embedded)
-print(text_embedded)
 sequence_size = 30
-text_embedded_tensor = torch.Tensor([])
+text_embedded_tensor = []
 i = 0
-counter = 1
 while i < len(text_embedded):
-    text_embedded_tensor = torch.cat(text_embedded[i: counter * sequence_size])
-    i = sequence_size + 1
-    counter += 1
+    end_index = int(sequence_size * ((i / sequence_size) + 1))
+    text_embedded_tensor.append(text_embedded[i:end_index])
+    i = end_index
 
-print(text_embedded_tensor)
+for arr in text_embedded_tensor:
+    if len(arr) < 30:
+        while len(arr) < 30:
+            arr.append(-1)
 
-# MyRNN = SimpleRNN(len(dictionary), len(input), len(input))
-# print(MyRNN.forward(dictionary))
+text_embedded_tensor = torch.tensor(text_embedded_tensor)
+
+class CustomTextDataset(Dataset):
+    def __init__(self, text_data):
+        self.text_data = text_data
+    
+    def __len__(self):
+        return len(self.text_data)
+    
+    def __getitem__(self, idx):
+        return self.text_data[idx]
+    
+training_data = CustomTextDataset(text_embedded_tensor)
+test_data = CustomTextDataset(text_embedded_tensor)
+train_loader = DataLoader(training_data, batch_size=2, shuffle=False)
+test_loader = DataLoader(test_data, batch_size=2, shuffle=False)
+
+class SimpleRNN(nn.Module):
+    def __init__(self, vocab_size, embed_size, hidden_size):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.rnn = nn.RNN(embed_size, hidden_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size, vocab_size)
+    def forward(self, x):
+        x = self.embedding(x)
+        out, _ = self.rnn(x)
+        out = self.fc(out)
+        return out
+
+MyRNN = SimpleRNN(len(dictionary), len(text_embedded_tensor), len(text_embedded_tensor))
+
+epochs = 1
+for epoch in range(epochs):
+    data = train_loader
+    print(data)
+    prediction = MyRNN(text_embedded_tensor)
+    optimizier = optim.Adam(prediction.parameters(), lr=0.001)
+    optimizier.zero_grad()
+    e_loss = nn.CrossEntropyLoss()
+    loss = e_loss(prediction, test_data)
+    loss.backward()
+    optimizier.step()
+
+    print(f"At epoch {epoch}: train_error: {loss}")
+
